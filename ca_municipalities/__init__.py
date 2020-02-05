765cc5be8710a42fa44c8e3eb88d920b9b67face
Andrew Lindsay (mirabuck) <andrew@newmode.net>
2020-02-05 09:23:36 -0500

    ET-4282 : adding CSV url.

--- ca_municipalities/__init__.py
+++ ca_municipalities/__init__.py
@@ -7,6 +7,7 @@ class CanadianMunicipalities(CanadianJurisdiction):
     division_name = 'Canada'
     name = 'Canada municipal councils'
     url = 'https://docs.google.com/spreadsheets/d/1QY-3q6W2edSORTgWlh8og_TJRQl1TZ3b7_Dn9E_TrMw/edit#gid=0'
+    csv_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRrGXQy8qk16OhuTjlccoGB4jL5e8X1CEqRbg896ufLdh67DQk9nuGm-oufIT0HRMPEnwePw2HDx1Vj/pub?output=csv'
 
     def get_organizations(self):
         return []
